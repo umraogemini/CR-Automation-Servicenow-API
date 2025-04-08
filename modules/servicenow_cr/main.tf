3a35375f27dd52e3c0193c202e3b2bab240620cd
umraogemini <umraogemini@gmail.com>
2025-04-08 21:39:51 +0530

    Update main.tf

--- modules/servicenow_cr/main.tf
+++ modules/servicenow_cr/main.tf
@@ -1,9 +1,5 @@
-// File: modules/servicenow_cr/main.tf
-resource "servicenow_change_request" "cr" {
-  short_description = var.short_description
-  description       = var.description
-  risk              = var.risk
-  impact            = var.impact
-  assignment_group  = var.assignment_group
-  requested_by      = var.requested_by
+// File: modules/servicenow_cr/outputs.tf
+output "cr_id" {
+  description = "The ID of the created Change Request"
+  value       = servicenow_change_request.cr.sys_id
 }
