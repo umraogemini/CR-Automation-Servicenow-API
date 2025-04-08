9eca78c277ca066493670b33a495ee340ae56708
umraogemini <umraogemini@gmail.com>
2025-04-08 17:00:36 +0530

    Update variables.tf

--- modules/servicenow_cr/variables.tf
+++ modules/servicenow_cr/variables.tf
@@ -1,25 +1,21 @@
-## Module Variables - Defines module input variables
+// Module Variables - Defines input variables for Minion API CR module
 variable "short_description" {
   description = "Short description for the CR"
   type        = string
+  default     ="Automation CR creation Using ServiceNow"
 }
 
 variable "description" {
   description = "Detailed description for the CR"
   type        = string
-}
-
-variable "category" {
-  description = "Category of the CR"
-  type        = string
-  default     = "Software"
+  default     ="Automated Change Request creation and appoval via terraform. A Github pull request triggers the CR request,While Jenkins manges approval and send email notification"
 }
 
 variable "risk" {
   description = "Risk level of the CR"
   type        = string
   default     = "low"
-}
+  }
 
 variable "impact" {
   description = "Impact level of the CR"
@@ -30,9 +26,11 @@ variable "impact" {
 variable "assignment_group" {
   description = "The assignment group for the CR"
   type        = string
+  default     ="ET-FINEX-BFF-PEAK-IT"
 }
 
-variable "requested_by" {
-  description = "The user who requested the CR"
-  type        = string
+Variable "requested_by"{
+  description  = "The useer who requested the CR'
+  type         = string
+  default        ="uma.rao@noexternalmail.hsbc.com"
 }
