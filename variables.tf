afe93cf0b97e5a715d0c1d50081c7fa1f5a440e4
umraogemini <umraogemini@gmail.com>
2025-04-08 17:32:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,32 @@
-## Global Variable - Defines input variables for Terraform
+// Global Variable - Defines input variables for Terraform
 variable "servicenow_instance" {
   description = "ServiceNow instance URL"
   type        = string
+  default     = "https://hsbcitidu.service-now.com/servicenow"
 }
 
 variable "servicenow_username" {
   description = "ServiceNow API username"
   type        = string
   sensitive   = true
+  default     = "Will be set GCP Secrate Manager"
 }
 
 variable "servicenow_password" {
   description = "ServiceNow API password"
   type        = string
   sensitive   = true
+  default     = "Will be set GCP Secrate Manager"
 }
 
 variable "assignment_group" {
   description = "Assignment group for CR"
   type        = string
-  default     = "ITSM Team"
+  default     = "ET-FINEX-BFF-PEAK-IT"
+}
+
+variable "requested_by" {
+  description = "User requesting the CR"
+  type        = string
+  default     = "uma.rao@noexternalmail.hsbc.com"
 }
