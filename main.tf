1ad4089ab8315fc67a8901fb134fcc2d25ce6eed
umraogemini <umraogemini@gmail.com>
2025-04-08 21:41:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
-// Terraform Root Module to Call CR Module - Calls the reusable CR module
+// File: main.tf
 module "create_cr" {
   source            = "./modules/servicenow_cr"
   short_description = "Automated CR via Terraform"
   description       = "Created for deployment automation"
-  category         = "Software"
-  risk            = "low"
-  impact          = "low"
-  assignment_group = var.assignment_group
-  requested_by    = var.requested_by
+  risk              = "low"
+  impact            = "low"
+  assignment_group  = var.assignment_group
+  requested_by      = var.requested_by
 }
 
 output "cr_id" {
