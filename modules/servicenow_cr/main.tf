f5c47a5798c2e7943780002826e9199a9b8f378d
umraogemini <umraogemini@gmail.com>
2025-04-08 21:39:33 +0530

    Update main.tf

--- modules/servicenow_cr/main.tf
+++ modules/servicenow_cr/main.tf
@@ -1,35 +1,9 @@
-// Minion API Integration for CR Create CR using Minion API - Calls Minion API to create & approve CR
-resource "http_request" "create_cr" {
-  url = "${var.minion_api_url}/create-cr"
-  method = "POST"
-
-  request_headers = {
-    Content-Type  = "application/json"
-    Authorization = "Bearer ${var.minion_api_token}"
-  }
-
-  request_body = jsonencode({
-    short_description = var.short_description
-    description       = var.description
-    risk             = var.risk
-    impact           = var.impact
-    assignment_group = var.assignment_group
-  })
-}
-
-# 🔹 Approve CR using Minion API
-resource "http_request" "approve_cr" {
-  url = "${var.minion_api_url}/approve-cr"
-  method = "POST"
-
-  request_headers = {
-    Content-Type  = "application/json"
-    Authorization = "Bearer ${var.minion_api_token}"
-  }
-
-  request_body = jsonencode({
-    cr_id = http_request.create_cr.response_body.cr_id
-  })
-
-  depends_on = [http_request.create_cr]
+// File: modules/servicenow_cr/main.tf
+resource "servicenow_change_request" "cr" {
+  short_description = var.short_description
+  description       = var.description
+  risk              = var.risk
+  impact            = var.impact
+  assignment_group  = var.assignment_group
+  requested_by      = var.requested_by
 }
