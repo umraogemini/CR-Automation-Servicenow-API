c17e86ec1e079748658761821b949813bebcf8ad
umraogemini <umraogemini@gmail.com>
2025-04-09 06:34:27 +0530

    Update outputs.tf

--- modules/servicenow_cr/outputs.tf
+++ modules/servicenow_cr/outputs.tf
@@ -1,5 +1,5 @@
-// File: outputs.tf
+// File: modules/servicenow_cr/outputs.tf
 output "cr_id" {
-  description = "Change Request ID"
-  value       = module.create_cr.cr_id
+  description = "The ServiceNow Change Request ID created via the HTTP request"
+  value       = jsondecode(http_request.create_cr.response_body).result.sys_id
 }
