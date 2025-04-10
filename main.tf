d0aaf57df127f93abc401c429ffdd450823cf8dc
umraogemini <umraogemini@gmail.com>
2025-04-10 11:30:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ module "create_cr" {
   source            = "./modules/servicenow_cr"
   short_description = "Automated CR via Terraform"
   description       = "Created for deployment automation"
-  category          = "Software"
   risk              = "low"
   impact            = "low"
   assignment_group  = var.assignment_group
