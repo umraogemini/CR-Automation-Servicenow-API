88ddececfadcb1653580d39c69d029170113266e
umraogemini <umraogemini@gmail.com>
2025-04-10 10:56:53 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,20 +1,10 @@
 terraform {
   required_providers {
-    servicenow = {
-      source  = "servicenow/servicenow"
-      version = "~> 0.3.0"
-    }
     http = {
       source  = "hashicorp/http"
       version = "3.4.5"
     }
   }
-}
-
-provider "servicenow" {
-  instance  = var.servicenow_instance
-  username  = var.servicenow_username
-  password  = var.servicenow_password
 }
 
 provider "http" {}
